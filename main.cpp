43d23b78741ba2b867c3b3e346af61ec5787ee06
Carl Woodward <cdwsignup@gmail.com>
2020-11-08 11:19:04 -0800

    Initial working version.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
-#include <iostream>
+#include <windows.h>
+#include <shlwapi.h>
+#pragma comment(lib, "shlwapi.lib")
+#include <filesystem>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+int main(int, char**) 
+{
+    const auto ghidra_env_path = R"(%USERPROFILE%\AppData\Local\Programs\Ghidra\ghidraRun.bat)";
+    CHAR ghidra_path[MAX_PATH + 1]{0};
+    ExpandEnvironmentStringsA(ghidra_env_path, ghidra_path, sizeof(ghidra_path) - 1);
+    SHELLEXECUTEINFOA si{
+        sizeof(si),
+        0, 
+        HWND_DESKTOP,
+        "Open",
+        ghidra_path, 
+        NULL,
+        NULL,
+        SW_SHOWNORMAL 
+    };
+    return ShellExecuteExA(&si);
 }
